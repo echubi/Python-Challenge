a944d8614b2997026cc9a63b1ddde392fedc03c6
echubi <echubiojo@gmail.com>
2021-01-21 15:37:35 +0100

    Update Combine two files.py

--- Learning-Plan/Combine two files.py
+++ Learning-Plan/Combine two files.py
@@ -1,5 +1,19 @@
-file = 'C:\\Users\\Echubi\\Documents\\file1.txt'
-fil = 'C:\\Users\\Echubi\\Documents\\file2.txt'
-file1 = open(file, 'r')
-file2 = open(fil, 'r')
+file1 = 'C:\\Users\\Echubi\\Documents\\file1.txt'
+file2 = 'C:\\Users\\Echubi\\Documents\\file2.txt'
+
+
+data = data2 =""
+
+with open(file1) as fp:
+    data = fp.readlines()
+
+with open(file2) as fp:
+    data2 = fp.readlines()
+
+with open("C:\\Users\\Echubi\\Documents\\file 3.txt", 'w') as z:
+    for i in range(len(data)):
+        line = data[i].strip() + ' ' + data2[i]
+        z.write(line)
+
+
 
