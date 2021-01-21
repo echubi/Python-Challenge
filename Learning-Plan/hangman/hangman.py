e74a0ab5819e9c3175e9111d7b6b82599d2401f6
echubi <echubiojo@gmail.com>
2021-01-21 18:57:38 +0100

    Create hangman.py

---
+++ Learning-Plan/hangman/hangman.py
@@ -0,0 +1,9 @@
+import random
+words = ["fish", "chicken", "egg", "turkey", "goat-meat"]
+a = random.choice(words)
+print(a.upper())
+
+letter = input("Input a letter:")
+letter = letter.upper()
+
+
