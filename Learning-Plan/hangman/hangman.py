eed623bbb8ab6e2c5c1dda6d3b300db8cd92f0d6
echubi <echubiojo@gmail.com>
2021-01-22 18:05:22 +0100

    Update hangman.py

--- Learning-Plan/hangman/hangman.py
+++ Learning-Plan/hangman/hangman.py
@@ -1,9 +1,23 @@
 import random
-words = ["fish", "chicken", "egg", "turkey", "goat-meat"]
+words = ["fish", "chicken", "egg", "turkey", "meat"]
 a = random.choice(words)
-print(a.upper())
+a = a.upper()
+print(a)
 
 letter = input("Input a letter:")
 letter = letter.upper()
 
+if l in a:
+    k = a.index(l)
+    print("YES", +k)
+else:
+    print("NO")
 
+while l not in a:
+    l = input("Input a letter:")
+    l = l.upper()
+    if l in a:
+        k = a.index(l)
+        print("YES", +k)
+    else:
+        print("NO")
