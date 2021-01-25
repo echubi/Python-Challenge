9656a3fa9a46a9a5eeac36a1aec19d3179972daf
echubi <echubiojo@gmail.com>
2021-01-25 22:58:25 +0100

    Update hangman.py

--- Learning-Plan/hangman/hangman.py
+++ Learning-Plan/hangman/hangman.py
@@ -1,45 +1,53 @@
 import random
 
+words = ["fish", "chicken", "egg", "turkey", "meat"]
+
+
 def target_word():
-    words = ["fish", "chicken", "egg", "turkey", "meat"]
     a = random.choice(words)
-    a = a.upper()
-    return a
+    return a.upper()
+
 
 def dictionary():
     dic = {}
-    for i in target_word():
+    t_word = target_word()
+    for i in t_word:
         dic.update({i: 'False'})
-    print(dic)
+
     return dic
 
-#dictionary()
 
-l = input("Input a letter(To end, enter '!'):")
+word = input("Input a letter(To end, enter '!'):").upper()
 
-def logic(l):
+
+def logic(let):
+
+
     guess = 0
-    while len(l) > 1:
-        l = input("Input a letter only:")
-    if l == '!':
-        exit()
-    l = l.upper()
+    diction = dictionary()
 
-    if l in target_word():
-        k = target_word.index(l)
+    print(diction)
+    while len(let) > 1:
+        let = input("Input a letter only:").upper()
+    if let == '!':
+        print(dictionary())
+        exit()
+
+    if let in diction:
+        k = list(diction).index(let)
         print("YES", +k)
-        dictionary().update({l: "true"})
-        #print(dic)
+        dictionary().update({let: "true"})
     else:
         print("NO")
 
-    while l not in target_word():
-        guess = guess + 1
-        l = input("Input a letter:")
-        l = l.upper()
-        if l in target_word():
-            k = target_word().index(l)
+    while let not in diction:
+
+        let = input("Input a letter:").upper()
+        if let in target_word():
+            k = list(diction).index(let)
             print("YES", +k)
         else:
             print("NO")
-            
+        guess = guess + 1
+
+logic(word)
