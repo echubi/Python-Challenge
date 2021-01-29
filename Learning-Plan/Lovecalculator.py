6575be1c4db38e0c538675c96285f2ec5893cd46
echubi <echubiojo@gmail.com>
2021-01-29 13:38:55 +0100

    Update Lovecalculator.py

--- Learning-Plan/Lovecalculator.py
+++ Learning-Plan/Lovecalculator.py
@@ -3,13 +3,13 @@ second = input("Input your partner's name:")
 
 names = first + "loves" + second
 a = []
-sum = 0
+total = 0
 
 for i in names:
     x = names.count(i)
     a.append(x)
 
 for j in range(0, len(a)):
-    sum = sum +  a[j]
+    total = total + a[j]
 
-print("The love percentage is", sum, "%")
+print("The love percentage is", total, "%")
