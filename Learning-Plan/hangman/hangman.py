b9b3d46be974350e0c3d2db95564012c5297f0a0
echubi <echubiojo@gmail.com>
2021-02-08 18:35:45 +0100

    Update hangman.py

--- Learning-Plan/hangman/hangman.py
+++ Learning-Plan/hangman/hangman.py
@@ -1,3 +1,4 @@
+"""
 import random
 import re
 
@@ -99,5 +100,100 @@ word = input("Input a letter(To end, enter '!'):").upper()
 guess_word(word)
 
 hangman()
+"""
 
+import random
+import re
+
+words = ["chicken", "fish", 'meat', 'turkey']
+
+guessed_letters = []
+correct = ''
+guesses = 10
+failed = 0
+guessed = False
+
+
+def target_word():
+    a = random.choice(words)
+    print(a)
+    return a.upper()
+
+
+class Hangman:
+    def __init__(self, target):
+        self.target = target
+
+    def dictionary(self):
+        dic = {}
+
+        for i in self.target:
+            dic.update({i: 'False'})
+        return dic
+
+    def guess_word(self):
+        global word, failed, guesses
+
+        if len(word) > 1:
+            word = input("Input a letter only:").upper()
+        if word == '!' or guesses == 0:
+            print("You made", failed, "failed attempts")
+            print("The correct word is", self.target)
+            exit()
+
+    def display_current_guess(self):
+        global display, guessed
+        for i in range(len(self.target)):
+            if self.target[i] in correct:
+                display = display[:i] + self.target[i] + display[i + 1:]
+        for letter in display:
+            print(letter, end=' ')
+        print()
+        if "_" not in display:
+            guessed = True
+        if guessed is True:
+            print("You won. The correct word was", self.target)
+            exit()
+
+    def run(self):
+        global guesses, failed, word, guessed_letters, correct
+
+        while self.target.find(word) != -1:
+            for i in guessed_letters:
+                for j in self.dictionary():
+                    if self.dictionary()[j] == word or i == word:
+                        print("Already guessed that. Try again")
+                        word = input("Input another letter: ").upper()
+                        self.guess_word()
+                        self.run()
+            k = [i.start() for i in re.finditer(word, self.target)]
+            print("YES", str(k))
+            self.dictionary().update({word: "True"})
+            correct = correct + word
+            self.display_current_guess()
+            guessed_letters.append(word)
+            word = input("Input another letter: ").upper()
+            self.guess_word()
+            self.run()
+
+        while self.target.find(word) == -1:
+            print("NO")
+            self.display_current_guess()
+            failed = failed + 1
+            guesses = guesses - 1
+            guessed_letters.append(word)
+            print(guesses, "more")
+            word = input("Input another letter: ").upper()
+            self.guess_word()
+            self.run()
+
+
+t_word = target_word()
 print(t_word)
+display = "_" * len(t_word)
+print(display)
+print("You have", guesses, "guesses")
+word = input("Input a letter(To end, enter '!'):").upper()
+hangman = Hangman(t_word)
+hangman.guess_word()
+hangman.run()
